a669ca486729db7d8260ee0a4b589aa60d96ee87
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-28 19:41:49 -0400

    All tests passing. Played with a decorator, deccy, to build my knowledge of decorators again.

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,15 @@
-def parrot():
-    pass
+# def deccy(func):
+#     def wrapper(*args, **kwargs):
+#         print("Before func call")
+#         result = func(*args, **kwargs)
+#         print("After func call")
+#         return result
+#     return wrapper
+
+# @deccy
+def parrot(stringy="Squawk!") -> str:
+    print(stringy)
+    return stringy
+
+if __name__ == "__main__":
+    print(parrot())
